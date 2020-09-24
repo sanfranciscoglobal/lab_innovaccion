6e6e65e8e8ccf6cbbd463c02439c9cc75be30a3c
PedroAltamirano <pedroaal04@gmail.com>
2020-09-24 11:33:02 -0500

    models

---
+++ app/Models/Fondo.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Fondo extends Model
+{
+    use SoftDeletes;
+
+    protected $table = 'fondos';
+
+    public function user(){
+        return $this->belongsTo('App\User');
+    }
+}
