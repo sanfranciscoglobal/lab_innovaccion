155b0d1ba38874d527e169a4dc7771a4a2b97dd3
jhonatan saguay saguay <jhonsaguay@gmail.com>
2020-09-30 23:35:22 -0500

    Eventos2

--- database/migrations/2020_09_30_222815_create_eventos_table.php
+++ database/migrations/2020_09_30_222815_create_eventos_table.php
@@ -15,8 +15,17 @@ class CreateEventosTable extends Migration
     {
         Schema::create('eventos', function (Blueprint $table) {
             $table->id();
-            
-            
+            $table->string('Nombre');
+            $table->string('Organizador');
+            $table->date('Fecha');
+            $table->string('Hora');
+            $table->string('Imagen');
+            $table->string('Descripcion');
+            $table->boolean('Tipo');
+            $table->string('Canton')->nullable();
+            $table->string('Ubicacion')->nullable();
+            $table->string('URL')->nullable();
+            $table->boolean('Estado');            
             $table->foreignId('user_id')->constrained();
             $table->timestamps();
             $table->softDeletes();
