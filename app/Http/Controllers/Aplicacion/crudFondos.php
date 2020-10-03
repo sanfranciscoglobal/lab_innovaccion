d891156ecebbb5ddf7c2b78cca21a4b2bb3323d9
PedroAltamirano <pedroaal04@gmail.com>
2020-10-02 22:23:33 -0500

    crud fondos

--- app/Http/Controllers/Aplicacion/crudFondos.php
+++ app/Http/Controllers/Aplicacion/crudFondos.php
@@ -25,36 +25,6 @@ class crudFondos extends Controller
      * App\Models\Fondo
      */
     public function store(StorePost $request){
-        
-        $fondo = Fondo::create($request);
-        // if($fondo = Fondo::create($request)){
-        //     if($request->imagen != null){
-        //         $extension = $request->imagen->extension();
-        //         $name = CustomUrl::slugify($request->organizacion).'_'.$id.'_'.date('Ymd').'_'.date('His');
-        //         $imageName = $name.'.'.$request->imagen->extension();
-        //         $fondo->image = $imageName;
-        //         $fondo->save();
-
-        //         /**
-        //          * GUARDAR IMAGEN EN SERVIDOR
-        //          */
-
-        //         $filename = "{$name}.{$extension}";
-        //         $path = ProcessImageSmall::returnPathImage();
-        //         try{
-        //             $request->imagen->move($path, $filename);
-        //         } catch (Exception $e) {
-        //             return redirect()->back()->withErrors('No hemos podido guardar tu imagen.');
-        //         }
-
-        //         ProcessImageSmall::dispatch($name, $extension, $path);
-        //     }
-
-        //     return redirect()->route('home')->with('status', 'Fondo creado con éxito');
-        // }
-
-        return redirect()->route('home')->with('status', 'Fondo creado con éxito');
-
         $validatedData = $request->validated();
         if($fondo = Fondo::create($validatedData)){
             // if($request->imagen != null){
@@ -82,7 +52,6 @@ class crudFondos extends Controller
             return redirect()->route('app.home')->with('status', 'Fondo creado con éxito');
         }
         return back()->with('error', 'Fondo no creado');
-
     }
 
     /**
