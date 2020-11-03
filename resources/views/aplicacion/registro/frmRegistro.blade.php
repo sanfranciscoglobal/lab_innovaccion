5b7b932eb328ca377153dc2fab38eb45744b796f
PedroAltamirano <pedroaal04@gmail.com>
2020-11-03 18:53:01 -0500

    celular required

--- resources/views/aplicacion/registro/frmRegistro.blade.php
+++ resources/views/aplicacion/registro/frmRegistro.blade.php
@@ -35,8 +35,9 @@
                             </div>
                             <div class="col-sm-4">
                                 <div class="form-group">
-                                    <label for="account-ln">* Celular</label>
-                                    <input class="form-control" type="text" id="account-ln" value="{{ old('celular', $perfil->celular) }}" name="celular" pattern="\+(9[976]\d|8[987530]\d|6[987]\d|5[90]\d|42\d|3[875]\d|2[98654321]\d|9[8543210]|8[6421]|6[6543210]|5[87654321]|4[987654310]|3[9643210]|2[70]|7|1)\d{1,14}$" required>
+                                    <label for="account-ln">Celular</label>
+                                    <input class="form-control" type="text" id="account-ln" value="{{ old('celular', $perfil->celular) }}" name="celular" pattern="\+(9[976]\d|8[987530]\d|6[987]\d|5[90]\d|42\d|3[875]\d|2[98654321]\d|9[8543210]|8[6421]|6[6543210]|5[87654321]|4[987654310]|3[9643210]|2[70]|7|1)\d{1,14}$">
+                                    <span class="muted-text">Ex. +5930987654321</span>
                                     @error('celular')<div class="invalid-feedback d-inline">{{ $message }}</div>@enderror
                                 </div>
                             </div>
