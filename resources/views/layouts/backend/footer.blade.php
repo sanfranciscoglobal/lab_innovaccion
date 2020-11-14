35fc977828f7d2e8005683855564ae704c882f3b
Paul Auqui <paul87.1987@gmail.com>
2020-11-14 11:32:23 -0500

    Template admin

--- resources/views/layouts/backend/footer.blade.php
+++ resources/views/layouts/backend/footer.blade.php
@@ -1,6 +1,83 @@
-<footer class="cs-footer bg-dark bg-purple-gradient pt-3 pt-md-3">
+<footer class="cs-footer bg-dark bg-purple-gradient pt-5 pt-md-6">
     <div class="container pt-3 pt-md-0">
-        <div class="row align-items-center my-3 pt-4">
+        <div class="row">
+            <div class="col-md-4 mt-n2 pb-3 pb-md-0">
+                <a class="d-block text-white mx-auto" href="index.html" style="width: 153px; color: white">
+                    <img src="{{asset('img/logo/thinkia_white.svg')}}" width="120" alt="Thinkia"/>
+                    <!--
+                    <h4 class="cs-widget-title text-white">Laboratorio de Innovación</h4>
+                    -->
+                </a>
+                <p class="font-size-sm text-light opacity-60 pb-2 pb-sm-3 mt-3">¡Conectate con otros actores, comparte tus retos e innova de manera colectiva!</p>
+            </div>
+            <div class="col-md-2 col-sm-4 ml-auto pb-1">
+                <div class="cs-widget cs-widget-light">
+                    <h4 class="cs-widget-title">Síguenos</h4>
+                    <a class="social-btn sb-outline sb-facebook sb-light sb-lg mr-2 mb-2" href="#">
+                        <i class="fe-facebook"></i>
+                    </a>
+                    <a class="social-btn sb-outline sb-twitter sb-light sb-lg mr-2 mb-2" href="#">
+                        <i class="fe-twitter"></i>
+                    </a>
+                    <a class="social-btn sb-outline sb-instagram sb-light sb-lg mr-2 mb-2" href="#">
+                        <i class="fe-instagram"></i>
+                    </a>
+                </div>
+            </div>
+            <div class="col-md-2 col-sm-4 ml-auto pb-1">
+                <div class="cs-widget cs-widget-light">
+                    <h4 class="cs-widget-title">Contáctanos</h4>
+                    <a class="text-white" href="mailto:hola@thinkia.org">hola@thinkia.org</a>
+                </div>
+            </div>
+            <div class="col-md-4 col-sm-4 ml-auto pb-1">
+                <div class="cs-widget cs-widget-light">
+                    <p class=" text-light opacity-60">Esta obra está bajo  Licencia Creative Commons Atribución 4.0 internacional.</p>
+                    <img src="{{asset('img/cc-badge.png')}}" width="100" alt="Creative Commons Attribution 3.0 International">
+                </div>
+            </div>
+            <?php /*
+            <div class="col-md-2 col-sm-4 ml-auto pb-1 mb-4">
+                <div class="cs-widget cs-widget-light">
+                    <h4 class="cs-widget-title">Iniciativas</h4>
+                    <ul>
+                        <li><a class="cs-widget-link" href="/mapeo/">Mapeo</a></li>
+                        <li><a class="cs-widget-link" href="/nuevas-iniciativas/">Nuevas iniciativas</a></li>
+                        <li><a class="cs-widget-link" href="/analitica/">Analítica</a></li>
+                    </ul>
+                </div>
+            </div>
+            <div class="col-lg-2 col-md-3 col-sm-4 pb-1 mb-4">
+                <div class="cs-widget cs-widget-light">
+                    <h4 class="cs-widget-title">Gestión de innovación</h4>
+                    <ul>
+                        <li><a class="cs-widget-link" href="/identificacion-de-problemas/">Identificación de problemas</a></li>
+                        <li><a class="cs-widget-link" href="/soluciones-innovadoras/">Soluciones innovadoras</a></li>
+                    </ul>
+                </div>
+            </div>
+            <div class="col-lg-2 col-md-3 col-sm-4 mb-4">
+                <div class="cs-widget cs-widget-light">
+                    <h4 class="cs-widget-title">Analítica</h4>
+                    <ul>
+                        <li><a class="cs-widget-link" href="/graficos/">Gráficos</a></li>
+                        <li><a class="cs-widget-link" href="/datos/">Datos</a></li>
+                    </ul>
+                </div>
+            </div>
+            <div class="col-lg-2 col-md-3 col-sm-4 mb-4">
+                <div class="cs-widget cs-widget-light">
+                    <h4 class="cs-widget-title">Recursos</h4>
+                    <ul>
+                        <li><a class="cs-widget-link" href="/fondos/">Fondos</a></li>
+                        <li><a class="cs-widget-link" href="/eventos/">Eventos</a></li>
+                        <li><a class="cs-widget-link" href="/material-de-aprendizaje/">Material de aprendizaje</a></li>
+                    </ul>
+                </div>
+            </div>
+            */ ?>
+        </div>
+        <!-- <div class="row align-items-center my-3 pt-4">
             <div class="col-md-6 order-md-2 text-md-right mb-3">
                 <ul class="list-inline font-size-sm mb-0">
                     <li class="list-inline-item"><a class="nav-link-style nav-link-light" href="#">Síguenos</a></li>
@@ -13,7 +90,7 @@
                             class="nav-link-style nav-link-light" href="#" target="_blank"
                             rel="noopener">Fundación San Francisco Global</a></p>
             </div>
-        </div>
+        </div> -->
     </div>
 </footer>
 <!-- Back to top button-->
