81789c2ab81edaca73edb6363c3d83aa826eadc7
PedroAltamirano <pedroaal04@gmail.com>
2020-11-26 17:32:51 -0500

    location based

--- resources/views/ar-test.blade.php
+++ resources/views/ar-test.blade.php
@@ -6,6 +6,10 @@
 		<script src="https://raw.githack.com/donmccurdy/aframe-extras/master/dist/aframe-extras.loaders.min.js"></script>
 		<script src="https://raw.githack.com/AR-js-org/studio-backend/master/src/modules/marker/tools/gesture-detector.js"></script>
 		<script src="https://raw.githack.com/AR-js-org/studio-backend/master/src/modules/marker/tools/gesture-handler.js"></script>
+
+		{{-- location based --}}
+		<script src="https://unpkg.com/aframe-look-at-component@0.8.0/dist/aframe-look-at-component.min.js"></script>
+		<script src="https://raw.githack.com/AR-js-org/AR.js/master/aframe/build/aframe-ar-nft.js"></script>
 	</head>
 
 	<body style="margin: 0; overflow: hidden">
@@ -43,6 +47,54 @@
 			</a-marker>
 
 			<a-entity camera></a-entity>
+		</a-scene>
+
+		<a-scene
+				renderer="logarithmicDepthBuffer: true;"
+				embedded
+				loading-screen="enabled: false;"
+				arjs="sourceType: webcam; debugUIEnabled: false;"
+		>
+			<a-assets>
+				<a-asset-item
+					id="animated-asset"
+					src="assets/asset.glb"
+				></a-asset-item>
+			</a-assets>
+
+			<a-entity
+				look-at="[gps-camera]"
+				animation-mixer="loop: repeat"
+				gltf-model="#animated-asset"
+				scale="0.1 0.1 0.1"
+				gps-entity-place="latitude: -0.116913; longitude: -78.499079;"
+			></a-entity>
+
+			<a-camera gps-camera rotation-reader></a-camera>
+		</a-scene>
+
+		<a-scene
+				renderer="logarithmicDepthBuffer: true;"
+				embedded
+				loading-screen="enabled: false;"
+				arjs="sourceType: webcam; debugUIEnabled: false;"
+		>
+			<a-assets>
+				<a-asset-item
+					id="animated-asset"
+					src="assets/asset.glb"
+				></a-asset-item>
+			</a-assets>
+
+			<a-entity
+				look-at="[gps-camera]"
+				animation-mixer="loop: repeat"
+				gltf-model="#animated-asset"
+				scale="0.1 0.1 0.1"
+				gps-entity-place="latitude: 0.40267; longitude: -78.1750884;"
+			></a-entity>
+
+			<a-camera gps-camera rotation-reader></a-camera>
 		</a-scene>
 	</body>
 </html>
