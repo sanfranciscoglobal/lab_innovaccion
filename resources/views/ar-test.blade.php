9e63629f52d0f51f1c7344d08d68275a6f7d51cb
PedroAltamirano <pedroaal04@gmail.com>
2020-11-26 21:59:13 -0500

    ar

--- resources/views/ar-test.blade.php
+++ resources/views/ar-test.blade.php
@@ -10,6 +10,9 @@
 		{{-- location based --}}
 		<script src="https://unpkg.com/aframe-look-at-component@0.8.0/dist/aframe-look-at-component.min.js"></script>
 		<script src="https://raw.githack.com/AR-js-org/AR.js/master/aframe/build/aframe-ar-nft.js"></script>
+
+		{{-- <script src="https://cdn.jsdelivr.net/gh/aframevr/aframe@1c2407b26c61958baa93967b5412487cd94b290b/dist/aframe-master.min.js"></script> --}}
+		{{-- <script src="https://cdn.jsdelivr.net/gh/aframevr/aframe@1c2407b26c61958baa93967b5412487cd94b290b/dist/aframe-master.min.js"></script> --}}
 	</head>
 
 	<body style="margin: 0; overflow: hidden">
@@ -17,7 +20,7 @@
 			vr-mode-ui="enabled: false;"
 			loading-screen="enabled: false;"
 			renderer="logarithmicDepthBuffer: true;"
-			arjs="trackingMethod: best; sourceType: webcam; debugUIEnabled: false;"
+			arjs="trackingMethod: best; sourceType: webcam; debugUIEnabled: false; videoTexture: true;"
 			id="scene"
 			embedded
 			gesture-detector
@@ -26,11 +29,11 @@
 				<a-asset-item id="animated-asset" src="{{asset('AR/Chaly_dance_002.gltf')}}"></a-asset-item>
 			</a-assets>
 
-			<a-marker
+			<!--a-marker
 				id="animated-marker"
 				type="pattern"
 				preset="custom"
-				url="{{asset('AR/marker.patt')}}"
+				url="{asset('AR/marker.patt')}}"
 				raycaster="objects:.clickable"
 				emitevents="true"
 				cursor="fuse:false;rayOrigin:mouse;"
@@ -44,25 +47,45 @@
 					class="clickable"
 					gesture-handler
 				></a-entity>
-			</a-marker>
+			</a-marker-->
 
 			<a-entity
 				look-at="[gps-camera]"
 				animation-mixer="loop: repeat"
 				gltf-model="#animated-asset"
 				scale="0.1 0.1 0.1"
-				gps-entity-place="latitude: -0.116913; longitude: -78.499079;"
+				minDistance="1"
+				maxDistance="10"
+				gps-projected-entity-place="latitude: -0.1169952; longitude: -78.4992004;"
 			></a-entity>
 
-			<a-entity
+			<!--a-entity
 				look-at="[gps-camera]"
 				animation-mixer="loop: repeat"
 				gltf-model="#animated-asset"
 				scale="0.1 0.1 0.1"
 				gps-entity-place="latitude: 0.40267; longitude: -78.1750884;"
-			></a-entity>
+			></a-entity-->
 
-			<a-entity camera gps-camera rotation-reader></a-entity>
+			<!--a-nft
+				type="nft"
+				url="{ asset('AR/lap') }}"
+				smooth="true"
+				smoothCount="10"
+				smoothTolerance=".01"
+				smoothThreshold="5"
+			>
+				<!- as a child of the a-nft entity, you can define the content to show. here's a GLTF model entity ->
+				<a-entity
+					gltf-model="#animated-asset"
+					scale="0.1 0.1 0.1"
+					position="0 0 0"
+				>
+				</a-entity>
+			</a-nft-->
+
+			<a-entity gps-projected-camera rotation-reader></a-entity>
+			{{-- <a-entity camera></a-entity> --}}
 		</a-scene>
 	</body>
 </html>
