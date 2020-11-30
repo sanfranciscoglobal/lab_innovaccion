6d72f449cd742c3bdbc64bd42360b36a7a2b906a
martinvelezf <martinvelezf@hotmail.com>
2020-11-29 19:53:24 -0500

    mapa responsive

--- resources/views/layouts/aplicacion/header.blade.php
+++ resources/views/layouts/aplicacion/header.blade.php
@@ -95,9 +95,10 @@
             <div class="cs-offcanvas-body">
                 <!-- Menu-->
                 <ul class="navbar-nav">
-                    <li class="nav-item">
-                        <a class="nav-link justify-content-center center-block text-center font-weight-bold" href="/gestion-innovacion" data-toggle="dropdown">Gestión de la Innovación</a>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link font-weight-bold" href="/gestion-innovacion">Gestión de la Innovación</a>
                     </li>
+
                     <li class="nav-item dropdown">
                         <a class="nav-link justify-content-center center-block text-center font-weight-bold" href="#" data-toggle="dropdown">Ecosistema de innovación</a>
                         <ul class="dropdown-menu">
