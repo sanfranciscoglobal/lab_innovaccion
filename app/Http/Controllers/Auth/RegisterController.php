31296ebda637461dc880e60c0ad3af02108a78f5
PedroAltamirano <pedroaal04@gmail.com>
2021-02-26 12:30:57 -0500

    mail

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -74,7 +74,7 @@ class RegisterController extends Controller
 
         Auth::attempt($request->only('email', 'password'));
 
-        // Mail::to($user->email)->send(new EmailVerification($user));
+        Mail::to($user->email)->send(new EmailVerification($user));
 
         return redirect()->route('home')->with('status', 'Usuario creado con éxito, por favor verifica tu correo electrónico.');
     }
