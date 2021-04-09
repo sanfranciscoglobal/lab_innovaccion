2b5d836dddc029b2b438b501e3097a8bacff8c13
PedroAltamirano <pedroaal04@gmail.com>
2021-04-09 09:25:32 -0500

    added tipo

--- database/seeds/MaterialTipoDocumentoSeeder.php
+++ database/seeds/MaterialTipoDocumentoSeeder.php
@@ -17,13 +17,13 @@ class MaterialTipoDocumentoSeeder extends Seeder
         $sql .= 'TRUNCATE material_tipodocumentos CASCADE;';
         DB::connection()->getPdo()->exec($sql);
 
-        MaterialTipodocumento::create(['nombre' => 'Artículo científico']);
-        MaterialTipodocumento::create(['nombre' => 'Blog']);
-        MaterialTipodocumento::create(['nombre' => 'Informe/Reporte']);
-        MaterialTipodocumento::create(['nombre' => 'Libro']);
-        MaterialTipodocumento::create(['nombre' => 'Manual']);
-        MaterialTipodocumento::create(['nombre' => 'Metodología']);
-        MaterialTipodocumento::create(['nombre' => 'Noticia']);
-       
+        MaterialTipodocumento::create(['nombre' => 'Artículo científico', 'tipo' => 1]);
+        MaterialTipodocumento::create(['nombre' => 'Blog', 'tipo' => 1]);
+        MaterialTipodocumento::create(['nombre' => 'Informe/Reporte', 'tipo' => 1]);
+        MaterialTipodocumento::create(['nombre' => 'Libro', 'tipo' => 0]);
+        MaterialTipodocumento::create(['nombre' => 'Manual', 'tipo' => 0]);
+        MaterialTipodocumento::create(['nombre' => 'Metodología', 'tipo' => 0]);
+        MaterialTipodocumento::create(['nombre' => 'Noticia', 'tipo' => 1]);
+
     }
 }
