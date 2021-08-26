8e18d9c6ee48bb739a5e93c3ec908d6ebff9947d
Carlos Gallardo <csgallardof@gmail.com>
2021-08-26 05:28:45 -0500

    fix banner

--- config/common_sliders_landing.php
+++ config/common_sliders_landing.php
@@ -219,7 +219,7 @@ return [
             'content_classes'           => 'd-flex flex-column justify-content-center align-items-center text-center',
             'text_color'                => 'white',
             'background_color'          => 'black',
-            'background_image_url'      => 'img/landing/banner_cambio_climantico_03_03.jpg',
+            'background_image_url'      => 'img/landing/banner_cambio_climantico_03_03.png',
             'background_blend_mode'     => '',
             'background_position'       => 'center',
             'background_size'           => 'cover'
@@ -237,7 +237,7 @@ return [
             'content_classes'           => 'd-flex flex-column justify-content-center align-items-center text-center',
             'text_color'                => 'white',
             'background_color'          => 'black',
-            'background_image_url'      => 'img/landing/banner_cambio_climantico_04_04.jpg',
+            'background_image_url'      => 'img/landing/banner_cambio_climantico_04_04.png',
             'background_blend_mode'     => '',
             'background_position'       => 'center',
             'background_size'           => 'cover'
